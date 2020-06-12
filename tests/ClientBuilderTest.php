c4a7f95c2ce45416da38babb41f4d5cebf9fdce5
Jérémy DECOOL <contact@jdecool.fr>
2020-06-12 22:53:38 +0200

    Move ClientBuilderTest to pestphp

--- tests/ClientBuilderTest.php
+++ tests/ClientBuilderTest.php
@@ -4,119 +4,96 @@ declare(strict_types=1);
 
 namespace JDecool\Clubhouse\Tests;
 
-use JDecool\{
-    Clubhouse\Client,
-    Clubhouse\ClientBuilder
+use JDecool\Clubhouse\{
+    Client,
+    ClientBuilder,
 };
-use PHPUnit\Framework\TestCase;
 use RuntimeException;
 
-class ClientBuilderTest extends TestCase
-{
-    public function testCreateClientV1(): void
-    {
-        $builder = new ClientBuilder();
-        $client = $builder->createClientV1('foo');
+test('create v1 client api', function(): void {
+    $builder = new ClientBuilder();
+    $client = $builder->createClientV1('foo');
 
-        $this->assertInstanceOf(Client::class, $client);
-    }
+    $this->assertInstanceOf(Client::class, $client);
+});
 
-    public function testAnExceptionThrowWhenCreateV1ClientTokenWithoutApiToken(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception throw when create v1 client api without token', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->createClientV1('');
-    }
+    $builder->createClientV1('');
+});
 
-    public function testCreateClientV2(): void
-    {
-        $builder = new ClientBuilder();
-        $client = $builder->createClientV2('foo');
+test('create v2 client api', function(): void {
+    $builder = new ClientBuilder();
+    $client = $builder->createClientV2('foo');
 
-        $this->assertInstanceOf(Client::class, $client);
-    }
+    $this->assertInstanceOf(Client::class, $client);
+});
 
-    public function testAnExceptionThrowWhenCreateV2ClientTokenWithoutApiToken(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception throw when create v2 client api without token', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->createClientV2('');
-    }
+    $builder->createClientV2('');
+});
 
-    public function testCreateClientV3(): void
-    {
-        $builder = new ClientBuilder();
-        $client = $builder->createClientV3('foo');
+test('create v3 client api', function(): void {
+    $builder = new ClientBuilder();
+    $client = $builder->createClientV3('foo');
 
-        $this->assertInstanceOf(Client::class, $client);
-    }
+    $this->assertInstanceOf(Client::class, $client);
+});
 
-    public function testAnExceptionThrowWhenCreateV3ClientTokenWithoutApiToken(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception throw when create v3 client api without token', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->createClientV3('');
-    }
+    $builder->createClientV3('');
+});
 
-    public function testCreateClientBeta(): void
-    {
-        $builder = new ClientBuilder();
-        $client = $builder->createClientBeta('foo');
+test('create beta client api', function(): void {
+    $builder = new ClientBuilder();
+    $client = $builder->createClientBeta('foo');
 
-        $this->assertInstanceOf(Client::class, $client);
-    }
+    $this->assertInstanceOf(Client::class, $client);
+});
 
-    public function testAnExceptionThrowWhenCreateBetaClientTokenWithoutApiToken(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception throw when create beta client api without token', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->createClientBeta('');
-    }
+    $builder->createClientBeta('');
+});
 
-    /**
-     * @dataProvider versions
-     */
-    public function testCreateClient(string $version): void
-    {
-        $builder = new ClientBuilder();
-        $client = $builder->create($version, 'foo');
+test('create client', function($version): void {
+    $builder = new ClientBuilder();
+    $client = $builder->create($version, 'foo');
 
-        $this->assertInstanceOf(Client::class, $client);
-    }
+    $this->assertInstanceOf(Client::class, $client);
+})->with([
+    ClientBuilder::V1,
+    ClientBuilder::V2,
+    ClientBuilder::V3,
+    ClientBuilder::BETA,
+]);
 
-    public function testCreateClientWithInvalidVersion(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception throw if trying to create a client with invalid version', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->create('foo', 'bar');
-    }
+    $builder->create('foo', 'bar');
+});
 
-    public function testAnExceptionThrowWhenCreateClientTokenWithoutApiToken(): void
-    {
-        $builder = new ClientBuilder();
+test('an exception thow when create client without api token', function(): void {
+    $builder = new ClientBuilder();
 
-        $this->expectException(RuntimeException::class);
+    $this->expectException(RuntimeException::class);
 
-        $builder->create(ClientBuilder::V3, '');
-    }
-
-    public function versions(): array
-    {
-        return [
-            [ClientBuilder::V1],
-            [ClientBuilder::V2],
-            [ClientBuilder::V3],
-            [ClientBuilder::BETA],
-        ];
-    }
-}
+    $builder->create(ClientBuilder::V3, '');
+});
