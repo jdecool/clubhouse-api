e5e049fa2d084571eb66256250e9d6ed7eecd5ff
Jérémy DECOOL <contact@jdecool.fr>
2020-09-26 15:42:18 +0200

    Use httplug plugins to avoid configuration management in client

--- src/Client.php
+++ src/Client.php
@@ -13,59 +13,22 @@ use JDecool\Clubhouse\{
 };
 use Http\Client\Common\HttpMethodsClientInterface;
 use Psr\Http\Message\ResponseInterface;
-use RuntimeException;
 
 class Client implements HttpClient
 {
-    private const ENDPOINT_V1 = 'https://api.clubhouse.io/api/v1';
-    private const ENDPOINT_V2 = 'https://api.clubhouse.io/api/v2';
-    private const ENDPOINT_V3 = 'https://api.clubhouse.io/api/v3';
-    private const ENDPOINT_BETA = 'https://api.clubhouse.io/api/beta';
-
     private $http;
-    private $baseUri;
-    private $token;
 
-    public static function createV1(HttpMethodsClientInterface $http, string $token): self
+    public function __construct(HttpMethodsClientInterface $http)
     {
-        return new self($http, self::ENDPOINT_V1, $token);
-    }
-
-    public static function createV2(HttpMethodsClientInterface $http, string $token): self
-    {
-        return new self($http, self::ENDPOINT_V2, $token);
-    }
-
-    public static function createV3(HttpMethodsClientInterface $http, string $token): self
-    {
-        return new self($http, self::ENDPOINT_V3, $token);
-    }
-
-    public static function createBeta(HttpMethodsClientInterface $http, string $token): self
-    {
-        return new self($http, self::ENDPOINT_BETA, $token);
-    }
-
-    public function __construct(HttpMethodsClientInterface $http, string $baseUri, string $token)
-    {
-        if (false === filter_var($baseUri, FILTER_VALIDATE_URL)) {
-            throw new RuntimeException('Invalid Clubouse base URI.');
-        }
-
-        if ('' === trim($token)) {
-            throw new RuntimeException('API token is required.');
-        }
-
         $this->http = $http;
-        $this->baseUri = $baseUri;
-        $this->token = $token;
     }
 
+    /**
+     * @throws ClubhouseException
+     */
     public function get(string $uri): array
     {
-        $response = $this->http->get(
-            $this->endpoint($uri)
-        );
+        $response = $this->http->get($uri);
 
         if (200 !== $response->getStatusCode()) {
             throw $this->createExceptionFromResponse($response);
@@ -74,14 +37,13 @@ class Client implements HttpClient
         return json_decode($response->getBody()->getContents(), true);
     }
 
+    /**
+     * @throws ClubhouseException
+     * @throws \JsonException
+     */
     public function post(string $uri, array $data): array
     {
-        // @phpstan-ignore-next-line
-        $response = $this->http->post(
-            $this->endpoint($uri),
-            ['Content-Type' => 'application/json'],
-            json_encode($data)
-        );
+        $response = $this->http->post($uri, [], json_encode($data, JSON_THROW_ON_ERROR));
 
         if (201 !== $response->getStatusCode()) {
             throw $this->createExceptionFromResponse($response);
@@ -90,14 +52,13 @@ class Client implements HttpClient
         return json_decode($response->getBody()->getContents(), true);
     }
 
+    /**
+     * @throws ClubhouseException
+     * @throws \JsonException
+     */
     public function put(string $uri, array $data): array
     {
-        // @phpstan-ignore-next-line
-        $response = $this->http->put(
-            $this->endpoint($uri),
-            ['Content-Type' => 'application/json'],
-            json_encode($data)
-        );
+        $response = $this->http->put($uri, [], json_encode($data, JSON_THROW_ON_ERROR));
 
         if (200 !== $response->getStatusCode()) {
             throw $this->createExceptionFromResponse($response);
@@ -106,25 +67,16 @@ class Client implements HttpClient
         return json_decode($response->getBody()->getContents(), true);
     }
 
+    /**
+     * @throws ClubhouseException
+     */
     public function delete(string $uri): void
     {
-        $response = $this->http->delete(
-            $this->endpoint($uri)
-        );
+        $response = $this->http->delete($uri);
 
         if (204 !== $response->getStatusCode()) {
             throw $this->createExceptionFromResponse($response);
         }
-    }
-
-    private function endpoint(string $uri): string
-    {
-        return sprintf(
-            '%s/%s?token=%s',
-            rtrim($this->baseUri, '/'),
-            ltrim($uri, '/'),
-            $this->token
-        );
     }
 
     private function createExceptionFromResponse(ResponseInterface $response): ClubhouseException
--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -6,11 +6,18 @@ namespace JDecool\Clubhouse;
 
 use Http\{
     Client\Common\HttpMethodsClient,
+    Client\Common\Plugin\AddHostPlugin,
+    Client\Common\Plugin\AddPathPlugin,
+    Client\Common\Plugin\AuthenticationPlugin,
+    Client\Common\Plugin\HeaderSetPlugin,
+    Client\Common\PluginClient,
     Client\HttpClient,
     Discovery\Psr17FactoryDiscovery,
     Discovery\Psr18ClientDiscovery,
+    Message\Authentication\QueryParam,
     Message\RequestFactory,
 };
+use Psr\Http\Message\UriFactoryInterface;
 use RuntimeException;
 
 class ClientBuilder
@@ -20,13 +27,23 @@ class ClientBuilder
     public const V3 = 'v3';
     public const BETA = 'beta';
 
+    private const ENDPOINT_V1 = 'https://api.clubhouse.io/api/v1';
+    private const ENDPOINT_V2 = 'https://api.clubhouse.io/api/v2';
+    private const ENDPOINT_V3 = 'https://api.clubhouse.io/api/v3';
+    private const ENDPOINT_BETA = 'https://api.clubhouse.io/api/beta';
+
     private $httpClient;
     private $requestFactory;
+    private $uriFactory;
 
-    public function __construct(?HttpClient $httpClient = null, ?RequestFactory $requestFactory = null)
-    {
+    public function __construct(
+        ?HttpClient $httpClient = null,
+        ?RequestFactory $requestFactory = null,
+        ?UriFactoryInterface $uriFactory = null
+    ) {
         $this->httpClient = $httpClient ?? Psr18ClientDiscovery::find();
         $this->requestFactory = $requestFactory ?? Psr17FactoryDiscovery::findRequestFactory();
+        $this->uriFactory = $uriFactory ?? Psr17FactoryDiscovery::findUriFactory();
     }
 
     public function createClientV1(string $token): Client
@@ -51,22 +68,48 @@ class ClientBuilder
 
     public function create(string $version, string $token): Client
     {
-        $http = new HttpMethodsClient($this->httpClient, $this->requestFactory);
-
         switch ($version) {
             case self::V1:
-                return Client::createV1($http, $token);
+                $url = self::ENDPOINT_V1;
+                break;
 
             case self::V2:
-                return Client::createV2($http, $token);
+                $url = self::ENDPOINT_V2;
+                break;
 
             case self::V3:
-                return Client::createV3($http, $token);
+                $url = self::ENDPOINT_V3;
+                break;
 
             case self::BETA:
-                return Client::createBeta($http, $token);
+                $url = self::ENDPOINT_BETA;
+                break;
+
+            default:
+                throw new RuntimeException("Version '$version' is not supported.");
         }
 
-        throw new RuntimeException("Version '$version' is not supported.");
+        $plugins = [
+            new AuthenticationPlugin(
+                new QueryParam(['token' => $token])
+            ),
+            new AddHostPlugin(
+                $this->uriFactory->createUri($url)
+            ),
+            new AddPathPlugin(
+                $this->uriFactory->createUri($url)
+            ),
+            new HeaderSetPlugin([
+                'User-Agent' => 'github.com/jdecool/clubhouse-api',
+                'Content-Type' => 'application/json',
+            ])
+        ];
+
+        $http = new HttpMethodsClient(
+            new PluginClient($this->httpClient, $plugins),
+            $this->requestFactory
+        );
+
+        return new Client($http);
     }
 }
